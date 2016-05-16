10b6abd2fbb096d7ceafa696362ccb4462f0c252
Rich Newman <rich-newman@user.noreply.github.com>
2016-05-16 22:28:44 +0100

    Initial commit

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using RestSharp;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Pdf()
+        {
+            var data = getPDF(@"http://localhost:3066", @"Example-PDF.pdf");
+            Response.Headers.Add("Content-Length", data.Length.ToString());
+            Response.Headers.Add("Content-Disposition", "inline; filename=Example-PDF.pdf");
+            return new FileContentResult(data, "application/pdf");
+        }
+
+        //using restsharp
+        private byte[] getPDF(string baseUrl, string pdfName)
+        {
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest(pdfName, Method.GET);
+            request.AddHeader("Accept", "*/*");
+            IRestResponse response = client.Execute(request);
+            return response.RawBytes;
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/demo.js
@@ -0,0 +1,46 @@
+﻿angular.module("Demo", []).controller("DemoCtrl", function ($scope, $http, $window, $q) {
+    var url = 'http://localhost:3066/Home/Pdf';
+    $scope.works = function () {
+        createPDFXMLHttpRequest(url, 'exampleToken');
+    }
+
+    $scope.doesntWork = function () {
+        createPDFHttp(url, 'exampleToken');
+    }
+
+    function createPDFXMLHttpRequest(url, token) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'arraybuffer';
+        xhr.setRequestHeader("token", token);
+        xhr.onload = function (e) {
+            if (this.status == 200) {
+                var file = new Blob([this.response], { type: 'application/pdf' });
+                var fileURL = URL.createObjectURL(file);
+                window.open(fileURL, '_blank');
+            }
+        };
+
+        xhr.send();
+    }
+
+    function createPDFHttp(url, token) {
+        var config = {
+            url: url,
+            method: 'GET',
+            responseType: 'arraybuffer',
+            headers: {
+                'Accept': '*/*',
+                "token": token
+            }
+        }
+
+        $http(config)
+            .success(function (response) {
+                var file = new Blob([response], { type: 'application/pdf' });
+                var fileURL = URL.createObjectURL(file);
+                window.open(fileURL, '_blank');
+            });
+    }
+
+});
